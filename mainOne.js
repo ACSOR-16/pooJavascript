1d9e5c969156edfc09875cdf7a0e033ab0701522
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-03-22 11:33:57 -0500

    abstaction

--- mainOne.js
+++ mainOne.js
@@ -1,15 +1,94 @@
-class LearningPath {
-  constructor({
-    
+// class classes \ constructor
+class ClassesOfCourse {
+  constructor ({
+    name,
+    classes = [],
   }) {
-
+    this,name = name;
+    this,classes = classes;
   }
 }
 
-const webSchool = new LearningPath();
-const designSchool = new LearningPath();
-const dataSchool = new LearningPath();
+// class Course \ constructor
+class Course {
+  constructor ({
+    name,
+    classes = [],
+  }) {
+    this,name = name;
+    this,classes = classes;
+  }
+}
 
+// instance of Courses
+
+const progBasicCourse = new Course({
+  name: "Basic programming Course",
+  classes: [],
+});
+const htmlCssCourse = new Course({
+  name: "Basic HTML and CSS course",
+  classes: [],
+});
+const basicJavascriptCourse = new Course({
+  name: "Basic Javascript course",
+  classes: [],
+});
+const animationCourse = new Course({
+  name: "Animation course",
+  classes: [],
+});
+const userExperience = new Course({
+  name: "User Experience course",
+  classes: [],
+});
+const dataBusinessCourse = new Course({
+  name: "Data Business course",
+  classes: [],
+});
+const dataBasesCourse = new Course({
+  name: "Data bases course",
+  classes: [],
+});
+
+// class LearningPath\ constructor
+class LearningPath {
+  constructor({
+    name,
+    courses = [],
+  }) {
+    this.name = name;
+    this.courses = courses;
+  }
+}
+
+// instance of learning path
+const webSchool = new LearningPath({
+  name: "Web School",
+  courses: [
+    progBasicCourse,
+    basicJavascriptCourse,
+    htmlCssCourse,
+  ],
+});
+const designSchool = new LearningPath({
+  name: "Design School",
+  courses: [
+    progBasicCourse,
+    animationCourse,
+    userExperience,
+  ],
+});
+const dataSchool = new LearningPath({
+  name: "Data Science School",
+  courses: [
+    progBasicCourse,
+    dataBusinessCourse,
+    dataBasesCourse,
+  ],
+});
+
+// class oneApprentice\ constructor
 class OneApprentice {
   constructor({
     name,
@@ -34,6 +113,7 @@ class OneApprentice {
   }
 }
 
+// instance of students \ oneApprentice
 const oscar = new OneApprentice({
   name: "oscar",
   email: "oscar@gmail.com",
@@ -52,4 +132,4 @@ const jef = new OneApprentice({
     webSchool,
     designSchool,
   ],
-})
+});
