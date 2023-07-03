2b18b09d405eb53974098f3baa2489daed45ff90
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-07-03 16:49:43 -0500

    deep copy con recursividad

--- POO intermedio/main2.js
+++ POO intermedio/main2.js
@@ -26,3 +26,59 @@ const stringifyComplexObj = JSON.stringify(objOne);
 // don't works with Methods
 const objTwo = JSON.parse(stringifyComplexObj);
 
+//Recursive
+// const numbers = [1,2,3,4,5,6,7,8,9,8];
+
+// function recursive(numbersArray) {
+//   if (numbersArray.length !== 0) {
+//     const first = numbersArray[0];
+//     console.log(first);
+//     numbersArray.shift();
+
+//     return recursive(numbersArray);
+
+//   } else {
+
+//   }
+// }
+
+function isArray(subject) {
+  return Array.isArray(subject);
+}
+function isObject(subject) {
+  return typeof subject == "object";
+}
+
+function deepCopy(subject) {
+  let copySubject;
+
+  let subjectIsArray = isArray(subject);
+  let subjectIsObject = isObject(subject);
+  
+  if (subjectIsArray) {
+    copySubject = [];
+
+  } else if (subjectIsObject) {
+    copySubject = {};
+  
+  } else {
+    return subject;
+  }
+
+  for (const key of subject) {
+    const keyIsObject = isObject(subject[key]);
+    
+    if (keyIsObject) {
+      copySubject[key] = deepCopy(subject[key]);
+
+    }else {
+      if (subjectIsArray) {
+        copySubject.push(subject[key]);
+      } else {
+        copySubject[key] = subject[key]
+      }
+    }
+  }
+   
+  return subject;
+}
